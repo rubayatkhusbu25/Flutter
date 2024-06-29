406e948531698b0830b989903595a9b2b8e8caca
Rubayat Islam <128803957+rubayatkhusbu25@users.noreply.github.com>
2024-06-29 15:23:15 +0600

    Update main.dart

--- main.dart
+++ main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.light(),
-      home: Dawer(),
+      home: Dawer(),  // this will be change for executing each part of codes
       debugShowCheckedModeBanner: false,
     );
   }
