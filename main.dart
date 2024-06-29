9bb5cc65019e00e0b0fd726bf408ac6d6b124558
Rubayat Islam <128803957+rubayatkhusbu25@users.noreply.github.com>
2024-06-29 15:22:01 +0600

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:practice/works/dawer.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      home: Dawer(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+
+
